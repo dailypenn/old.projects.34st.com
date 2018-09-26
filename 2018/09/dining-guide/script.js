d6dc8be8f5621a754e43b888d03925f9ed96d5cb
Alex Graves <graves@thedp.com>
2018-09-26 00:55:04 -0400

    adding a bunch of articles

--- 2018/09/dining-guide/script.js
+++ 2018/09/dining-guide/script.js
@@ -158,19 +158,13 @@ var geojson = {
   ]
 };
 
-var popups = [];
-
-function clearPopups() {
-  popups.forEach(function(el) {
-    el.remove();
-  });
-}
+const popups = [];
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZHB3ZWJkZXYiLCJhIjoiY2pmYmN2dDB4MWNoYjRlcTdjM2Y3bGpzZyJ9.1m44PhD4VmqEhKhI4FhPKA';
-var map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/dpwebdev/cjfbcjxzi69w52rk936tb9lyu',
-  center: [-75.2156881, 39.9483286],
+  center: [-75.1604066,39.9431914],
   zoom: 13,
   scrollZoom: false
 });
@@ -178,62 +172,69 @@ var map = new mapboxgl.Map({
 map.addControl(new mapboxgl.NavigationControl());
 
 // populate map
-map.on('load', function() {
+map.on('load', () => {
   map.addSource('data', {
     type: 'geojson',
     data: geojson
   });
 
   map.addLayer({
-    "id": "restaurants",
-    "type": "circle",
-    "source": "data"
+    id: 'restaurants',
+    type: 'circle',
+    paint: {
+      "circle-color": 'rgb(232,75,59)'
+    },
+    source: 'data'
+  });
+
+  // add listeners for popups when clicking on point
+  map.on('click', 'restaurants', e => {
+    const coordinates = e.features[0].geometry.coordinates.slice();
+    const prop = e.features[0].properties;
+    const text = `<h4>${prop.title}</h4><p class="address">${prop.address}</p><a href="${prop.url}" target="_blank">Read more »</a>`;
+
+    popups.push(new mapboxgl.Popup().setLngLat(coordinates).setHTML(text).addTo(map));
+  });
+
+  // change the cursor to a pointer when the mouse is over the current layer
+  map.on('mouseenter', name, () => {
+    map.getCanvas().style.cursor = 'pointer';
+  });
+
+  // change it back to a pointer when it leaves.
+  map.on('mouseleave', name, () => {
+    map.getCanvas().style.cursor = '';
   });
 });
 
 // add restaurants to sidebar
-for (var i in geojson.features) {
-  var prop = geojson.features[i].properties;
-  var listing = listings.appendChild(document.createElement('div'));
+for (let i in geojson.features) {
+  const prop = geojson.features[i].properties;
+
+  const listing = listings.appendChild(document.createElement('div'));
   listing.className = 'item';
   listing.position = i;
 
-  var title = listing.appendChild(document.createElement('h6'));
+  const title = listing.appendChild(document.createElement('h6'));
   title.className = 'title';
   title.innerHTML = prop.title;
 
-  var details = listing.appendChild(document.createElement('div'));
-  details.innerHTML = '<i>'+ prop.address +'</i>';
+  const details = listing.appendChild(document.createElement('div'));
+  details.innerHTML = `<p class="address">${prop.address}</p>`;
 
   // add click event listener for listings
   listing.addEventListener('click', function(e) {
-    var feature = geojson.features[this.position];
-    var text = '<h4>' + feature.properties.title + '</h4><a href="' +
-               feature.properties.url + '" target="_blank">Read more »</a>';
-    var coordinates = feature.geometry.coordinates;
+    const curr = geojson.features[this.position];
+    const prop = curr.properties;
+    const text = `<h4>${prop.title}</h4><p class="address">${prop.address}</p><a href="${prop.url}" target="_blank">Read more »</a>`;
+    const coordinates = curr.geometry.coordinates;
 
-    clearPopups();
+    // clear popups so we only have one on the map at once
+    popups.forEach(el => {
+      el.remove();
+    });
+
     map.setCenter(coordinates, 16);
-    var popup = new mapboxgl.Popup().setLngLat(coordinates).setHTML(text).addTo(map);
-    popups.push(popup);
+    popups.push(new mapboxgl.Popup().setLngLat(coordinates).setHTML(text).addTo(map));
   });
 }
-
-map.on('click', 'restaurants', function (e) {
-  var coordinates = e.features[0].geometry.coordinates.slice();
-  var prop = e.features[0].properties;
-  var text = '<h4>' + prop.title + '</h4><a href="' + prop.url + '" target="_blank">Read more »</a>';
-
-  var popup = new mapboxgl.Popup().setLngLat(coordinates).setHTML(text).addTo(map);
-  popups.push(popup);
-});
-
-// Change the cursor to a pointer when the mouse is over the places layer.
-map.on('mouseenter', 'restaurants', function () {
-  map.getCanvas().style.cursor = 'pointer';
-});
-
-// Change it back to a pointer when it leaves.
-map.on('mouseleave', 'restaurants', function () {
-  map.getCanvas().style.cursor = '';
-});
